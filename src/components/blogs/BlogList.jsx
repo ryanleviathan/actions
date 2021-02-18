25b61360fd9a90e9110139f53dded328d9259b1d
ryanleviathan <ryan.m.carreras@gmail.com>
2021-02-17 16:09:42 -0800

    added bloglist component

---
+++ src/components/blogs/BlogList.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { useSelector } from '../../state/BlogProvider';
+import { getBlogs } from '../../selectors/blogSelector';
+import Blog from './Blog';
+
+const BlogList = () => {
+  const blogs = useSelector(getBlogs);
+
+  const blogElements = blogs.map(blog => (
+    <li key={blog.title}>
+      <Blog {...blog} />
+    </li>
+  ));
+
+  return (
+    <ul>
+      {blogElements}
+    </ul>
+  );
+};
+
+export default BlogList;
